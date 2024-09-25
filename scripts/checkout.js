d67b08bd257e4510d5a008e443488fa16e200b14
Favour Uchechukwu <favouruche4000@gnail.com>
2024-09-25 09:38:51 +0100

    Made the delete button intractive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/carts.js';
+import {cart, removeFromCart} from '../data/carts.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 
@@ -42,9 +42,10 @@ Delivery date: Tuesday, June 21</div>
     <span class="update-quantity-link link-primary">
         Update
     </span>
-    <span class="delete-quantity-link link-primary">
-        Delete
-    </span>
+    <span class="delete-quantity-link link-primary 
+    js-delete-link" data-product-id="${matchingProduct.id}">
+    Delete
+     </span>
     </div>
 </div>
 
@@ -94,3 +95,11 @@ Delivery date: Tuesday, June 21</div>
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+  document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+      link.addEventListener('click', () => {
+       const productId = link.dataset.productId;
+       removeFromCart(productId);
+      });
+    });
