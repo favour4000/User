18e8302a28d2fff70df4233be019d8680b78d60f
Favour Uchechukwu <favouruche4000@gnail.com>
2024-09-25 08:58:03 +0100

    Fixed the radio select button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/carts.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from '../utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -30,8 +31,7 @@ Delivery date: Tuesday, June 21</div>
     ${matchingProduct.name}
     </div>
     <div class="product-price">
-    $${matchingProduct.priceCents /
-        100}
+    $${formatCurrency(matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
     <span> 
@@ -57,7 +57,7 @@ Delivery date: Tuesday, June 21</div>
         type="radio"
         checked
         class="delivery-option-input"
-        name="delivery-option-1"
+        name="delivery-option-${matchingProduct.id}"
     />
     <div>
         <div class="delivery-option-date">Tuesday, June 21</div>
@@ -68,7 +68,7 @@ Delivery date: Tuesday, June 21</div>
     <input
         type="radio"
         class="delivery-option-input"
-        name="delivery-option-1"
+        name="delivery-option-${matchingProduct.id}"
     />
     <div>
         <div class="delivery-option-date">Wednesday, June 15</div>
@@ -79,7 +79,7 @@ Delivery date: Tuesday, June 21</div>
     <input
         type="radio"
         class="delivery-option-input"
-        name="delivery-option-1"
+        name="delivery-option-${matchingProduct.id}"
     />
     <div>
         <div class="delivery-option-date">Monday, June 13</div>
